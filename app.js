27c051263c7b715a32bbae71007b6bc84002fe71
Peafly78 <90190882+Peafly78@users.noreply.github.com>
2024-06-15 13:58:07 +0200

    add logic to move obstacles on timeloop

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ document.addEventListener('DOMContentLoaded', () => {
     const timeLeft = document.querySelector('#time-left');
     const result = document.querySelector('#result');
     const startBtn = document.querySelector('#button');
+    const carsLeft = document.querySelector('car-left');
+    const carsRight = document.querySelector('car-right');
+    const logsLeft = document.querySelector('log-left');
+    const logsRight = document.querySelector('log-right');
     const width = 9;
     let currentIndex = 76;
     let timerId;
@@ -10,7 +14,7 @@ document.addEventListener('DOMContentLoaded', () => {
     // render frog on starting block
     squares[currentIndex].classList.add('frog');
 
-    // move the frog
+    // move frog
     function moveFrog(e) {
         squares[currentIndex].classList.remove('frog');
         switch(e.keyCode) {
@@ -31,4 +35,98 @@ document.addEventListener('DOMContentLoaded', () => {
         // lose();
         // win();
     }
+
+    // move cars
+    function autoMoveCars() {
+        carsLeft.forEach(carLeft => moveCarLeft(carLeft));
+        carsRight.forEach(carRight => moveCarRight(carRight));     
+    }
+
+    // move car left on a timeloop
+    function moveCarLeft(carLeft) {
+        switch (true) {
+            case carLeft.classList.contains('c1'):
+                carLeft.classList.remove('c1');
+                carLeft.classList.add('c2');
+                break;
+            case carLeft.classList.contains('c2'):
+                carLeft.classList.remove('c2');
+                carLeft.classList.add('c3');
+                break;
+            case carLeft.classList.contains('c3'):
+                carLeft.classList.remove('c3');
+                carLeft.classList.add('c1');
+                break;
+        }
+    }
+
+    // move car right on a timeloop
+    function moveCarRight(carRight) {
+        switch (true) {
+            case carRight.classList.contains('c1'):
+                carRight.classList.remove('c1');
+                carRight.classList.add('c2');
+                break;
+            case carRight.classList.contains('c2'):
+                carRight.classList.remove('c2');
+                carRight.classList.add('c3');
+                break;
+            case carRight.classList.contains('c3'):
+                carRight.classList.remove('c3');
+                carRight.classList.add('c1');
+                break;
+        }
+    }
+
+    // move log left on a timeloop
+    function moveLogLeft(logLeft) {
+        switch (true) {
+            case logLeft.classList.contains('l1'):
+                logLeft.classList.remove('l1');
+                logLeft.classList.add('l2');
+                break;
+            case logLeft.classList.contains('l2'):
+                logLeft.classList.remove('l2');
+                logLeft.classList.add('l3');
+                break;
+            case logLeft.classList.contains('l3'):
+                logLeft.classList.remove('l3');
+                logLeft.classList.add('l4');
+                break;
+            case logLeft.classList.contains('l4'):
+                logLeft.classList.remove('l4');
+                logLeft.classList.add('l5');
+                break;
+            case logLeft.classList.contains('l5'):
+                logLeft.classList.remove('l5');
+                logLeft.classList.add('l1');
+                break;
+        }
+    }
+    // move log right on a timeloop
+    function moveLogRight(logRight) {
+        switch (true) {
+            case logRight.classList.contains('l1'):
+                logRight.classList.remove('l1');
+                logRight.classList.add('l2');
+                break;
+            case logRight.classList.contains('l2'):
+                logRight.classList.remove('l2');
+                logRight.classList.add('l3');
+                break;
+            case logRight.classList.contains('l3'):
+                logRight.classList.remove('l3');
+                logRight.classList.add('l4');
+                break;
+            case logRight.classList.contains('l4'):
+                logRight.classList.remove('l4');
+                logRight.classList.add('l5');
+                break;
+            case logRight.classList.contains('l5'):
+                logRight.classList.remove('l5');
+                logRight.classList.add('l1');
+                break;
+        }
+    }
+
 });
